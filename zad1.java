f8a47a67f2b5bb20ffac98ea1dc5fa34da4eb3de
Konrad <ktercjak0@gmail.com>
2014-05-25 06:32:34 +0200

    zmieniono zmienna na 6

---
+++ zad1.java
@@ -0,0 +1,15 @@
+Public class TestIf{
+public static void main(String[] args){
+int zmienna = 6;
+if(zmienna%2 == 0)
+System.out.println("Liczba parzysta");
+else
+System.out.println("Liczba nieparzysta");
+if(zmienna>10)
+System.out.println("Liczba większa od 10");
+else if(zmienna>0 && zmienna<=10)
+System.out.println("Liczba dodatnia mniejsza, lub równa 10");
+else
+System.out.println("Liczba ujemna");
+}
+}
